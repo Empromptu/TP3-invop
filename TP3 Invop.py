9bb20af52fdffba5edaca92671c82b5d3399d150
Juan I <141973423+Empromptu@users.noreply.github.com>
2025-07-05 20:41:03 -0300

    Add files via upload
    agregue método Hooke & Jeeves

--- TP3 Invop.py
+++ TP3 Invop.py
@@ -88,3 +88,52 @@ ax.set_zlabel('Z')
 ax.legend()
 plt.tight_layout()
 plt.show()
+
+
+# funcion para correr dentro de Hooke y Jeeves
+def explorar(f, base, delta):
+    # ej depunto en R3: [1,2,3] 
+    punto = base.copy()
+    n = len(punto) 
+    for i in range(n): 
+        # evaluo cuanto vale el punto en f 
+        f_actual = f(punto) 
+        # hago un paso en la dimension i del punto [1 + delta, 2, 3] 
+        punto[i] += delta   
+        # evaluo el punto con el paso en f
+        f_nuevo = f(punto)   
+        # si el punto nuevo es mayor que el orignial, no me sirve.
+        if f_nuevo >= f_actual:
+            # pruebo con hacer el paso para el otro lado
+            # [1 - delta, 2, 3]
+            punto[i] -= 2 * delta
+            f_nuevo = f(punto)
+            # si tampoco es menor que el orignial lo descarto.
+            if f_nuevo >= f_actual:
+                punto[i] += delta  # vuelvo al punto [1,2,3]
+    return punto
+
+def hookejeeves(f,puntos, delta=0.5, alpha=2.0, epsilon=1e-5, max_iter=1000):
+    # elijo el punto del medio por ejemplo
+    x0 = puntos[len(puntos)//2].copy()  
+    xe = explorar(f,x0,delta)
+
+    i = 0
+    while delta > epsilon and i < max_iter:
+        
+        if f(xe) < f(x0):
+            # pattern move : Es un salto más largo en la dirección 
+            # de la mejora encontrada.
+            # porque si encontraste una dirección que mejora la función, tiene 
+            # sentido suponer que seguir avanzando en esa dirección podría seguir mejorando.
+            xp = [xe[i] + alpha * (xe[i] - x0[i]) for i in range(len(x0))]
+            x0 = xe.copy() # cambio el x0 por el nuevo punto (f(xe)<f(x0))
+            xe = explorar(f,xp,delta) # busco si el punto lejano es menor
+        
+        else:
+            delta = delta/2  # reduzco el tamanio del salto
+            xe = explorar(x0,delta) # busco en un punto mas cercano al x0
+
+        i += 1
+
+    return x0, f(x0)
